3965dc1893d701b919e5ea9d1118c20baf9eeaa3
MarchelliusF <106514764+MarchelliusF@users.noreply.github.com>
2023-09-23 19:18:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	_ "soedirman-digital-school/pertemuan_1"
-	_ "soedirman-digital-school/pertemuan_2"
+	_ "Tugas-SDS/pertemuan 1"
+	_ "Tugas SDS/pertemuan2"
 )
 
 func main() {
